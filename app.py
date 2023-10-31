278d2f37e240eba81bf92cd2e46134d4b9429982
akhilcabeus <149470280+akhilcabeus@users.noreply.github.com>
2023-10-31 04:04:20 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import spacy
+
+app = Flask(__name__)
+CORS(app) 
+
+# Load the pre-trained SpaCy model
+nlp_ner = spacy.load("model-best")
+
+@app.route('/ner', methods=['POST'])
+def extract_entities():
+    # Get data from the request
+    data = request.json
+    text = data.get("text")
+
+    # Make sure that text is provided
+    if not text:
+        return jsonify({"error": "No text provided for entity recognition."}), 400
+
+    # Process the text with the NER model
+    doc = nlp_ner(text)
+
+    # Extract entities from the doc
+    entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
+
+    # Return the entities as a JSON response
+    return jsonify(entities)
+
+if __name__ == '__main__':
+    app.run(debug=True)
